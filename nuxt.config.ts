96fb2285cd5a0aa5627b19b81ae899a730adc950
Zoltán Makai <70949528+ZoltanMakai@users.noreply.github.com>
2023-11-21 10:44:12 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt';
+
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  generate: {
+    routeRules: {
+      '/**': { prerender: true },
     },
   },
 });
